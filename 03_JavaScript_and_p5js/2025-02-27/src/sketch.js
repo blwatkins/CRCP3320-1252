38bbf5f9e9a9b5000a6ab790a2ea98d3bee51413
blwatkins <blwatkins@users.noreply.github.com>
2025-02-27 11:33:32 -0600

    Adding initial files for Feb 27 Notes

---
+++ 03_JavaScript_and_p5js/2025-02-27/src/sketch.js
@@ -0,0 +1,50 @@
+// Confetti sketch
+
+let pg;
+
+let generators = [];
+
+function setup() {
+    const c = createCanvas(500, 500);
+    
+    try {
+        c.parent('main-content');
+    } catch (error) {
+        console.warn(error);
+    }
+    
+    let x = 250;
+    let y = 100;
+    let pointTotal = 20;
+    let energy = 5;
+    let maxSize = 20;
+    pg = new ParticleGenerator(x, y, pointTotal, energy, maxSize, getRandomColor());
+}
+
+function draw() {
+    background(0);
+    pg.draw();
+
+    for (let g of generators) {
+        g.draw();
+    }
+}
+
+function mousePressed() {
+    let pointTotal = Math.floor(random(5, 50));
+    let energy = random(1, 10);
+    let maxSize = random(10, 25);
+    let generator = new ParticleGenerator(mouseX, mouseY, pointTotal, energy, maxSize, getRandomColor());
+    generators.push(generator);
+}
+
+function drawParticle(particle) {
+    particle.draw();
+}
+
+function getRandomColor() {
+    let r = Math.floor(random(255));
+    let g = Math.floor(random(255));
+    let b = Math.floor(random(255));
+    return color(r, g, b);
+}
